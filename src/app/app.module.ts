edd9df74fa9865840ecc4c838b341d122d2fbaa1
root <root@LAPTOP-P57IN275.localdomain>
2021-07-01 21:55:13 -0400

    * Created some components * Modified styles.css * Modified import's in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardReaderComponent } from './components/card-reader/card-reader.component';
+import { CashDispenserComponent } from './components/cash-dispenser/cash-dispenser.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
+import { ButtonLeftColumnComponent } from './components/left-side/button-left-column/button-left-column.component';
+import { ButtonRightColumnComponent } from './components/right/button-right-column/button-right-column.component';
+import { ScreenComponent } from './components/screen/screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent,
+    CardReaderComponent,
+    CashDispenserComponent,
+    KeyboardComponent,
+    ButtonRightColumnComponent,
+    ButtonLeftColumnComponent
   ],
   imports: [
     BrowserModule,
