7aed8625a260ff4814d5f92027d7cd4ee0e7e800
root <root@LAPTOP-P57IN275.localdomain>
2021-07-01 22:06:03 -0400

    * Created some components * Modified styles.css * Modified import's in AppModule

---
+++ src/app/components/right/button-right-column/button-right-column.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-right-column',
+  templateUrl: './button-right-column.component.html',
+  styleUrls: ['./button-right-column.component.css']
+})
+export class ButtonRightColumnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
