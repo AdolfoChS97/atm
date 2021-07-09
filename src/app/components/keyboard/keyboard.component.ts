17c632da467681afc5447fd42b3686131dcc654e
Adolfo Chafardett <adolfo1997a@gmail.com>
2021-07-09 03:56:03 -0400

    * Layouted keyboard * Defined styles of buttons * TO-DO assign a behavior

--- src/app/components/keyboard/keyboard.component.ts
+++ src/app/components/keyboard/keyboard.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyboardComponent implements OnInit {
 
+  public numberKeys: Array<Array<string>> = [ 
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9'],
+    ['-', '0', '+'],
+  ];
+
+  public actionKeys: Array<string> = ['', 'CANCEL', 'CLEAR', 'ENTER'];
+
   constructor() { }
 
   ngOnInit(): void {
